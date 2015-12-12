4d1558c3aed6c622d3ff8af00bdcc0a37dd37507
JamesMuir <JRMuir09@gmail.com>
2015-12-12 19:29:01 +0000

    Added logging

--- AI.py
+++ AI.py
@@ -1,12 +1,17 @@
 #AI
 import time
+import calendar
+import datetime
 
 repeating = True
 while repeating:
     userInput = input("Enter question: ").lower()
+    logging = open('logging.txt', 'a')
+    logging.write(userInput + "\n")
+    logging.close()
     if userInput.find("what") >= 0 and userInput.find("date") >= 0:
         print("Current date: {}".format(time.strftime("%d/%m/%Y")))
     if userInput.find("what") >= 0 and userInput.find("time") >= 0:
-        print("Current time is: {}".format(time.strftime("%H:%M:%S"))
-    if userInput.find("show") >= 0 and userInput.find("calendar")
-    
+        print("Current time is: {}".format(time.strftime("%H:%M:%S")))
+    if userInput.find("show") >= 0 and userInput.find("calendar"):
+        print(calendar.month(datetime.date.today().year, datetime.date.today().month))
