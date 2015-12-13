3683376a7789c266151fa490a96a59e5a2f733fd
JamesMuir <JRMuir09@gmail.com>
2015-12-13 22:42:18 +0000

    Continued 9. HOW MANY MINUTES
    Started taking user input

--- Python Workbook/9. HOW MANY MINUTES[2].py
+++ Python Workbook/9. HOW MANY MINUTES[2].py
@@ -2,4 +2,13 @@
 repeating = True
 
 while repeating:
-    
+    print("Please enter the number of days and hours you want converted into minutes.")
+    while True:
+        try:
+            numberOfDays = int(input("How many days: "))
+            numberOfHours = int(input("How many hours: "))
+        except ValueError:
+            print("Enter a numerical value.")
+        except:
+            print("How did you manage to brake something this badly???")
+            
