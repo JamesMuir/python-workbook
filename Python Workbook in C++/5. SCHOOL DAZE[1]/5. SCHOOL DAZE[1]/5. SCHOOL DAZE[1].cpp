dcd087ab11bc435cbf4aaf5e3f778aabc420e098
JamesMuir <JRMuir09@gmail.com>
2016-01-07 22:51:09 +0000

    Done number 4 in C++

---
+++ Python Workbook in C++/5. SCHOOL DAZE[1]/5. SCHOOL DAZE[1]/5. SCHOOL DAZE[1].cpp
@@ -0,0 +1,13 @@
+// 5. SCHOOL DAZE[1].cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int main()
+{
+	int hoursAtSchool = 6 * 192 * 5;
+	std::cout << "You spend " << hoursAtSchool << " hours at secondary school over five years." << std::endl;
+    return 0;
+}
+
