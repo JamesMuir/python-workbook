0388014c69f4663cc74977d77c8def0c31a9d5d2
JamesMuir <JRMuir09@gmail.com>
2016-01-10 22:20:07 +0000

    Added just the C++ and .exe files in a separate folder

---
+++ Python Workbook in C++ .cpp and .exe files only/1. Love List[1].cpp
@@ -0,0 +1,14 @@
+// 1. Love List[1].cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "CS:GO \nHockey \nProgramming \nWatching TV" << endl;
+    system("pause");
+    return 0;
+}
+
