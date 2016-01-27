168ebd8a2258fae554b7dceeb1484a8fe4f0d528
JamesMuir <JRMuir09@gmail.com>
2016-01-27 20:36:57 +0000

    Finished number 23

--- Python Workbook/23. USER NUMBER ENTRY[4].py
+++ Python Workbook/23. USER NUMBER ENTRY[4].py
@@ -6,14 +6,14 @@ while repeating:
     gettingData = True
     while gettingData:
         while True:
-            numberOne = int_input("Please enter a number between one and twenty: ")
+            numberOne = float_input("Please enter a number between one and twenty: ")
             if numberOne >= 1 and numberOne <= 20:
                 break
             else:
                 print()
                 
         while True:
-            numberTwo = int_input("Please enter a number between one and fifty: ")
+            numberTwo = float_input("Please enter a number between one and fifty: ")
             if numberTwo >= 1 and numberTwo <= 50:
                 break
             else:
@@ -33,10 +33,48 @@ while repeating:
 
     pickingOperation = True
     while pickingOperation:
-        operation = input("Do you want to add or multiply? \nEnter A or W: ").lower()
-        if operation == "a" or operation == "w":
+        while True:
+            operation = input("Do you want to add or multiply? \nEnter A or M: ").lower()
+            if operation == "a" or operation == "m":
+                break
+            else:
+                print("Enter A or M.\n")
+
+        if operation == "a":
+            print("You choose to add.")
+        elif operation == "m":
+            print("You choose to multiply.")
+
+        while True:
+            print()
+            pickingOperationData = input("Is the above information correct. Enter Y/N: ").lower()
+            if pickingOperationData == "y":
+                pickingOperation = False
+                break
+            elif pickingOperationData == "n":
+                break
+            else:
+                print("Enter Y/N.")
+                print()
+
+    if operation == "a":
+        output = numberOne + numberTwo
+        print("You two number added together are {}.".format(output))
+    elif operation == "m":
+        output = numberOne * numberTwo
+        print("You two number multiplied together are {}.".format(output))
+
+    print()
+    while True:
+        userRepeat = input("Do you want to repeat? \nEnter Y/N: ").lower()
+        if userRepeat == "y":
+            break
+        elif userRepeat == "n":
+            repeating = False
             break
         else:
-            print("Enter A or W.\n")
+            print("Enter valid input.")
+
+
 
     
