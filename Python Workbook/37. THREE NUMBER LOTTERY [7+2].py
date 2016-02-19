0b3f4737a61833668a9f6a53f9f83ad8715eaf80
JamesMuir <JRMuir09@gmail.com>
2016-02-19 13:32:49 +0000

    Finished TenNumbers

--- Python Workbook/37. THREE NUMBER LOTTERY [7+2].py
+++ Python Workbook/37. THREE NUMBER LOTTERY [7+2].py
@@ -1,6 +1,52 @@
 #THREE NUMBER LOTTERY [7]
+from random import randint
+from commonFunctions import int_input
+
 repeating = True
 while repeating:
     gettingData = True
-        while gettingData:
-            
+    while gettingData:
+        numbers = []
+        print("Please enter three different numbers between.")
+        for i in range(3):
+            numbers.append(None)
+            while True:
+                numbers[i] = int_input("Please enter number {}: ".format(i+1))
+                if numbers[i] >= 1 and numbers[i] <= 10:
+                    break
+                    
+        
+        if numbers[0] in numbers[1:]:
+            print("Please make sure the numbers are different.")
+        elif numbers[1] == numbers[0] or numbers[1] == numbers[2]:            
+            print("Please make sure the numbers are different.")
+        elif numbers[2] in numbers[:2]:
+            print("Please make sure the numbers are different.")
+        else:
+            print("You entered {}.".format(numbers))
+            while True:
+                print("Is this correct?")
+                isCorrect = input("Enter Y/N: ").lower()
+                if isCorrect == "y":
+                    gettingData = False
+                    break
+                elif isCorrect == "n":
+                    break
+        
+    
+    while True:
+        lottery = []
+        for i in range(3):
+            lottery.append(randint(1,10))
+
+        if lottery[0] in lottery[1:]:
+            pass
+        elif lottery[1] == lottery[0] or lottery[1] == lottery[2]:
+            pass
+        elif lottery[2] in lottery[:2]:
+            pass
+        else:
+            break
+
+    print(numbers)
+    print(lottery)
--- TenNumbers.py
+++ TenNumbers.py
@@ -1,14 +1,43 @@
 #TenNumbers
 from commonFunctions import float_input
 
+def mode(array):
+    most = max(list(map(array.count, array)))
+    return list(set(filter(lambda x: array.count(x) == most, array)))
+
 repeating = True
 while repeating:
     numbers = []
     for i in range(10):
-        numbers.append(float_input("Please enter a number: "))
-
-    print(numbers)
-
+        numbers.append(None)
+        while True:
+            numbers[i] = float_input("Please enter number {}: ".format(i+1))
+            if numbers[i] >= 0:
+                break
+            
     total = 0
     for i in range(10):
         total += numbers[i]
+
+    numbersSorted = sorted(numbers)
+    mean = total /10
+    median = (numbersSorted[4] + numbersSorted[5])/2
+    mode = mode(numbers)
+    theRange = numbersSorted[9] - numbersSorted[0]
+
+    print("The total is {}.".format(total))
+    print("The mean is {}.".format(mean))
+    print("The median is {}.".format(median))
+    print("The mode is {}.".format(mode))
+    print("The range is {}.".format(theRange))
+    
+    print()
+    while True:
+        userRepeat = input("Do you want to repeat? \nEnter Y/N: ").lower()
+        if userRepeat == "y":
+            break
+        elif userRepeat == "n":
+            repeating = False
+            break
+        else:
+            print("Enter valid input.")
