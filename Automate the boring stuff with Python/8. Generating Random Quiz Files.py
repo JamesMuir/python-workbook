85e8e0733d841573834041b888a5b62bb7a98792
JamesMuir <JRMuir09@gmail.com>
2016-03-03 22:14:56 +0000

    Started ATBSWP 8

---
+++ Automate the boring stuff with Python/8. Generating Random Quiz Files.py
@@ -0,0 +1,41 @@
+#! python3
+#Generating Random Quiz Files
+
+from random import shuffle
+
+# The quiz data. Keys are states and values are their capitals.
+capitals = {
+'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix',
+'Arkansas': 'Little Rock', 'California': 'Sacramento', 'Colorado': 'Denver',
+'Connecticut': 'Hartford', 'Delaware': 'Dover', 'Florida': 'Tallahassee',
+'Georgia': 'Atlanta', 'Hawaii': 'Honolulu', 'Idaho': 'Boise', 'Illinois':
+'Springfield', 'Indiana': 'Indianapolis', 'Iowa': 'Des Moines', 'Kansas':
+'Topeka', 'Kentucky': 'Frankfort', 'Louisiana': 'Baton Rouge', 'Maine':
+'Augusta', 'Maryland': 'Annapolis', 'Massachusetts': 'Boston', 'Michigan':
+'Lansing', 'Minnesota': 'Saint Paul', 'Mississippi': 'Jackson', 'Missouri':
+'Jefferson City', 'Montana': 'Helena', 'Nebraska': 'Lincoln', 'Nevada':
+'Carson City', 'New Hampshire': 'Concord', 'New Jersey': 'Trenton', 'NewMexico':
+'Santa Fe', 'New York': 'Albany', 'North Carolina': 'Raleigh',
+'North Dakota': 'Bismarck', 'Ohio': 'Columbus', 'Oklahoma': 'Oklahoma City',
+'Oregon': 'Salem', 'Pennsylvania': 'Harrisburg', 'Rhode Island': 'Providence',
+'South Carolina': 'Columbia', 'South Dakota': 'Pierre', 'Tennessee':
+'Nashville', 'Texas': 'Austin', 'Utah': 'Salt Lake City', 'Vermont':
+'Montpelier', 'Virginia': 'Richmond', 'Washington': 'Olympia', 'West Virginia':
+'Charleston', 'Wisconsin': 'Madison', 'Wyoming': 'Cheyenne'
+}
+
+# Generate 35 quiz files 
+for quizNum in range(35):
+    # Create the quiz and answer key files
+    quizFile = open("CapitalsQuiz{}.txt".format(quizNum + 1) , "w")
+    answerFile = open("CapitalsQuizAnswers{}.txt".format(quizNum + 1) , "w")
+
+    #Quiz file header
+    quizFile.write('Name:\n\nDate:\n\nPeriod:\n\n')
+    quizFile.write((' ' * 20) + 'State Capitals Quiz (Form %s)' % (quizNum + 1))
+    quizFile.write('\n\n')
+
+    #Shuffle the order of the states
+    states = list(capitals.keys())
+    shuffle(states)
+    
