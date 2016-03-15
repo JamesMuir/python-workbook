6ff9e08cf85a6a2262373ec2ebaec4dd33dde7f6
JamesMuir <JRMuir09@gmail.com>
2016-03-15 22:10:31 +0000

    Started 51

---
+++ Python Workbook/51. HEX-ELLENT [5].py
@@ -0,0 +1,6 @@
+#HEX-ELLENT [5]
+repeating = True
+while repeating:
+    gettingData = True
+    while gettingData:
+        number = input("Please enter a 2 digit hex number or 8 bit binary number: ")
