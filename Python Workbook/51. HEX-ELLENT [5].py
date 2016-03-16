109fc0748efe2f0af9bdd1b4730a1dba157364e4
JamesMuir <JRMuir09@gmail.com>
2016-03-16 21:30:51 +0000

    Finished 51 in python

--- Python Workbook/51. HEX-ELLENT [5].py
+++ Python Workbook/51. HEX-ELLENT [5].py
@@ -4,3 +4,49 @@ while repeating:
     gettingData = True
     while gettingData:
         number = input("Please enter a 2 digit hex number or 8 bit binary number: ")
+        if len(number) == 2:
+            try:
+                binaryNumber = bin(int(number, 16))
+                while True:
+                    print("You entered {}.".format(number))
+                    isDataCorrect = input("Is this correct? Enter Y/N: ").lower()
+                    
+                    if isDataCorrect == "y":
+                        gettingData = False
+                        break
+                    elif isDataCorrect == "n":
+                        break
+            except ValueError:
+                print("Enter a valid hexadecimal number.")
+                
+        elif len(number) == 8:
+            try:
+                hexNumber = hex(int(number, 2))
+                while True:
+                    print("You entered {}.".format(number))
+                    isDataCorrect = input("Is this correct? Enter Y/N: ").lower()
+                    
+                    if isDataCorrect == "y":
+                        gettingData = False
+                        break
+                    elif isDataCorrect == "n":
+                        break
+            except ValueError:
+                print("Enter a valid binary number.")
+    
+
+    if len(number) == 2:
+        print("{} in binary is {}.".format(number, binaryNumber[2:]))
+        
+    elif len(number) == 8:
+        print("{} in hex is {}.".format(number, hexNumber[2:]))
+
+    print()
+    while True:
+        print("Do you want to repeat?")
+        repeat = input("Enter Y/N: ").lower()
+        if repeat == "y":
+            break
+        elif repeat == "n":
+            repeating = False
+            break
