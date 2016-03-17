7ee9d3bdffbcde37e62402a10a85b6596f988fbe
JamesMuir <JRMuir09@gmail.com>
2016-03-17 21:56:48 +0000

    Finished 54 in python

--- Python Workbook/54. WHAT IS IN A NAME [2].py
+++ Python Workbook/54. WHAT IS IN A NAME [2].py
@@ -1,6 +1,6 @@
 #WHAT IS IN A NAME? [2]
 def nameComment(name):
-    nameUpper name.upper()
+    nameUpper = name.upper()
 
     if nameUpper == "JIM":
         print("All people called Jim suck.")
@@ -10,9 +10,34 @@ def nameComment(name):
         print("All people called Mary are potatos.")
     elif nameUpper == "KIM":
         print("All people called Kim are Kardishians.")
-
+    else:
+        print("People called {} are meh.".format(nameUpper))
+    
 repeating = True
 while repeating:
     gettingData = True
     while gettingData:
-        name = input("")
+        name = input("Please enter name: ")
+
+        print()
+        while True:
+            print("You entered {}.".format(name))
+            isDataCorrect = input("Is this correct? \nEnter Y/N: ").lower()
+
+            if isDataCorrect == "y":
+                gettingData = False
+                break
+            elif isDataCorrect == "n":
+                break
+
+    nameComment(name)
+    
+    print()
+    while True:
+        print("Do you want to repeat?")
+        repeat = input("Enter Y/N: ").lower()
+        if repeat == "y":
+            break
+        elif repeat == "n":
+            repeating = False
+            break
